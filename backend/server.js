78916bffa76fab2d79006375f6760d20698f2b0e
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-03 20:40:06 +0200

    Configuring cors and importing envirnment variables

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 // Importing Express and create an instance of the Express
 const express = require('express');
+const cors = require('cors');
+
+// Load .env variables
+require("dotenv").config({ path: __dirname + '/./.env' });
+
+const PORT = process.env.APP_PORT;
+
 const app = express();
 
 // Defining middleware functions to parse request bodies and handle CORS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+console.log(PORT);
