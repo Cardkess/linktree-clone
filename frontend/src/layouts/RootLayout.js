ff1b4aa656d16806be095338465fc874e812be91
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-04 19:50:21 +0200

    ADD: Base layout

--- frontend/src/layouts/RootLayout.js
+++ frontend/src/layouts/RootLayout.js
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+
 
 export default function RootLayout() {
   return (
-    <div className='root-layout'>
-       <main>
-        <Outlet />
-       </main>
+    <div className="root-layout">
+      <div className="root-container">
+        <nav className="nav-bar">
+          <div className="menu-toggle">
+            <p>...</p>
+          </div>
+        </nav>
+
+        <main>
+          <Outlet />
+        </main>
+
+        <footer className="app-footer"></footer>
+      </div>
     </div>
-  )
+  );
 }
