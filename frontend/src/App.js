b43880d31ba084351bc711dc88596e77bd2edd42
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-05 15:16:56 +0200

    ADD: Paages to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 
 // importing pages/screens
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 
 import RootLayout from "./layouts/RootLayout";
 
@@ -16,7 +19,11 @@ import {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout/>}>
+
       <Route index element={<Home/>}/>
+      <Route path="signup" element={<Signup/>}/>
+      <Route path="login" element={<Login/>}/>
+      
     </Route>
   )
 )
