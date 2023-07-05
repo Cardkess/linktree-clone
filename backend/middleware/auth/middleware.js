13c45a269a3be7c424f2f799279dec703ee4b813
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 00:15:02 +0200

    ADD: Token Auth Middleware

---
+++ backend/middleware/auth/middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication token is missing' });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+
+    req.userId = decodedToken.userId;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
