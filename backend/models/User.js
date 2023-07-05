0c41ff7ecb1b1d9277239ba14dda252d9eabe1bd
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-05 23:36:09 +0200

    ADD: Password Hashing and Compare

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,6 +16,29 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  try {
+    if (this.isModified('password')) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Compare the provided password with the stored hashed password
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
 const User = mongoose.model('User', userSchema);
 
