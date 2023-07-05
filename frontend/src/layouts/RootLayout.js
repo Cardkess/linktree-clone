9c00e4af2dbd6137493a4ea3c0c3ea4ab49d79fd
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-05 18:45:07 +0200

    Code refactoring

--- frontend/src/layouts/RootLayout.js
+++ frontend/src/layouts/RootLayout.js
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
-
+import Model from "../components/Model";
+import ToggleButton from "../components/ToggleButton";
 
 export default function RootLayout() {
+
+
   return (
     <div className="root-layout">
+
+      
+      <Model/>
+
       <div className="root-container">
+        
         <nav className="nav-bar">
-          <div className="menu-toggle">
-            <p>...</p>
-          </div>
+          <ToggleButton/>
         </nav>
 
         <main>
