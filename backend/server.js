fa3429b016e8672a1cfab5c37a7d5851a765f8a5
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 00:12:37 +0200

    MOD: File structure

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 
 // Load .env variables
 require("dotenv").config({ path: __dirname + '/./.env' });
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/users/routes');
+const authRoutes = require('./routes/auth/routes');
 
 // Connect to the MongoDB database
 mongoose.connect('mongodb://127.0.0.1:27017/linktree-clone', {
@@ -22,8 +23,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Use the user routes
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+// registering routes
 app.use('/api', userRoutes);
+app.use('/api/auth', authRoutes);
 
 // Check the MongoDB connection
 const db = mongoose.connection;
