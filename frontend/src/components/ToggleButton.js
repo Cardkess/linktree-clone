7f0b9c9c1bd19aeb54d6ec605c32eb12841b2267
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-05 21:12:59 +0200

    ADD: Navingatio Icon UI and Logic

--- frontend/src/components/ToggleButton.js
+++ frontend/src/components/ToggleButton.js
@@ -1,13 +1,34 @@
-import { useDispatch } from 'react-redux';
-import { toggle } from './toggleButtonSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggle } from "./toggleButtonSlice";
+import { TiArrowBack } from "react-icons/ti";
+import { BsThreeDots } from "react-icons/bs";
+
+import { useLocation, useNavigate  } from "react-router-dom";
 
 export default function ToggleButton() {
-
   const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [currentRoute, setCurrentRoute] = useState("");
+
+  useEffect(() => {
+    // This code will run whenever the current route changes
+    setCurrentRoute(location.pathname);
+  }, [location.pathname]); // Run the effect only when location.pathname changes
+
+  const navigation = () => {
+    if (currentRoute !== "/") {
+      navigate(-1); // Navigate to the previous page
+    } else {
+      dispatch(toggle());
+    }
+  };
 
   return (
-    <div onClick={() => dispatch(toggle())} className="menu-toggle">
-      <p>...</p>
+    <div onClick={ navigation } className="menu-toggle">
+      {currentRoute !== "/" && <TiArrowBack className="back-arrow" />}
+      {currentRoute === "/" && <BsThreeDots />}
     </div>
   );
 }
