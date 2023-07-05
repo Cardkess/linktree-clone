d66c40baa1d860f058d0f0d8fbc868d0236458f5
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-05 18:44:16 +0200

    ADD: Model

---
+++ frontend/src/components/Model.js
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import {AiOutlineClose} from 'react-icons/ai';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggle } from "./toggleButtonSlice";
+
+export default function Model() {
+
+  const visibilty = useSelector((state) => state.toggleButton.value);
+  const dispatch = useDispatch();
+
+  return (
+    
+    <div className={`model-container ${visibilty ? 'visible' : 'hidden'}`}>
+        <div className="popup-model">
+          <header>
+            <AiOutlineClose onClick={() => dispatch(toggle())} className='popup-close-icon'/>
+            <h4>Create your own Linktree</h4>
+            <span>The only link in bio trusted by 35M+ people.</span>
+          </header>
+          <hr/>
+
+          <div className="popup-model-links">
+            <NavLink onClick={() => dispatch(toggle())} to='login' className='popup-nav-link btn-login'>Login</NavLink>
+            <NavLink onClick={() => dispatch(toggle())} to='signup' className='popup-nav-link btn-signup'>Sign up</NavLink>
+          </div>
+          
+        </div>
+      </div>
+  )
+}
