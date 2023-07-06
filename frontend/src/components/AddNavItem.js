1cae0087a2796f1a4048e9db6c822356df71ca20
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 14:46:01 +0200

    ADD: Add link Item

---
+++ frontend/src/components/AddNavItem.js
@@ -0,0 +1,7 @@
+import {MdAddCircle} from 'react-icons/md';
+
+export default function NavItem() {
+  return (
+    <div className="nav-item"><div className='add-item-container-div'><MdAddCircle className='add-icon'/><span>Add Link</span></div></div>
+  )
+}
