f219e602a3b306b11b1f9c404570f98adc4ef888
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 10:04:21 +0200

    Refactoring Singup Error handler and Login

--- backend/routes/auth/routes.js
+++ backend/routes/auth/routes.js
@@ -27,10 +27,10 @@ router.post('/signup', async (req, res, next) => {
           const duplicateKey = Object.keys(error.keyValue)[0];
           const duplicateValue = error.keyValue[duplicateKey];
           
-          res.status(200).json({code:'400', error: `User with ${duplicateKey}: ${duplicateValue} already exists.` });
+          res.status(400).json({code:'400', error: `User with ${duplicateKey}: ${duplicateValue} already exists.` });
         } else {
           // Other error occurred
-          res.status(200).json({code:'500', error: 'An error occurred' });
+          res.status(500).json({code:'500', error: 'An error occurred' });
         }
       }
 });
@@ -38,19 +38,21 @@ router.post('/signup', async (req, res, next) => {
 // User login route
 router.post('/login', async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ code:'401', error: 'Authentication failed' });
     }
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      
+      return res.status(401).json({ code:'401', error: 'Authentication failed' });
     }
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
       expiresIn: '1h'
     });
-    res.json({ token });
+    return res.status(200).json({code:'200',  message: 'Authentication successful', token:token });
   } catch (err) {
     next(err);
   }
