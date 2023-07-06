9e9e3bfb980eb0b8e77dc2cad63f6e67343c6dd4
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 10:05:03 +0200

    ADD: Implementing login logic

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,10 @@
 import logo from "../tappa.png";
-import { Form } from "react-router-dom";
+import { Form, useActionData} from "react-router-dom";
 
-export default function login() {
+export default function Login() {
+  
+  const data = useActionData();
+
   return (
     <div>
       <div className="logo-container">
@@ -10,7 +13,7 @@ export default function login() {
 
       <div className="body-container">
         <h1>Sign in</h1>
-        <Form>
+        <Form method="post" action="/login">
           <div className="form-group">
             <label>Email:</label>
             <input type="email" id="email" name="email" required />
@@ -19,10 +22,48 @@ export default function login() {
             <label>Password:</label>
             <input type="password" id="password" name="password" required />
           </div>
-          <hr className="login-divider"/>
+          <hr className="login-divider" />
           <button className="login-submit-btn">Login</button>
         </Form>
+        {data && data.error && <span>{data.error}</span>}
+        {data && data.message && <span>{data.message}</span>}
       </div>
     </div>
   );
 }
+
+export const loginAction = async ({ request }) => {
+
+  const data = await request.formData();
+
+  const url = process.env.REACT_APP_BACKEND_API_URL + "/auth/login";
+
+  const submition = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(submition),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+
+      return data
+
+    } else {
+
+      const data = await response.json();
+
+      throw new Error(data.error);
+    }
+  } catch (error) {
+    return { error: error.message };
+  }
+};
