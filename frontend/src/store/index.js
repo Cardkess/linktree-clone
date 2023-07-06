34b1e0fffc087c4813cb69e7eff80f2d952ad369
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 13:43:21 +0200

    ADD: Registering user state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import toggleButtonReducer from './toggleButtonSlice';
+import userReducer from './userSlice';
 export default configureStore({
   reducer: {
     toggleButton:toggleButtonReducer,
+    user:userReducer,
   },
 })
