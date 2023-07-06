aac28fc2e043a0ec388d8275a8fb59335c804761
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 19:06:17 +0200

    ADD: Loading Data from server

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,8 +2,12 @@ import NavItem from "../components/NavItem";
 import AddNavItem from "../components/AddNavItem";
 import logo from "../tappa.png";
 import { useSelector } from "react-redux";
+import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
+
+  const links = useLoaderData();
+
   const user = useSelector((state) => state.user.value);
   return (
     <div>
@@ -16,10 +20,18 @@ export default function Home() {
         <p>South Africas Leading NFC smart business card manufacturer</p>
       </div>
 
-      {user.id && <AddNavItem/>}
-      <NavItem text='Visit Our Website'/>
-      <NavItem text='Get Your Smart Business Card'/>
-
+      {(user.id || links.length === 0) && <AddNavItem />}
+      {links.map( link => (
+        <NavItem link={link.url} text={link.title} key={link._id}/>
+      ))}
     </div>
   );
 }
+
+export const linksLoader = async ({ request }) => {
+  const url = process.env.REACT_APP_BACKEND_API_URL + "/links";
+
+  const res = await fetch(url);
+
+  return res.json();
+};
