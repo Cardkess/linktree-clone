5395958351d023f1441597fbf77e8afaba625762
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-06 14:57:25 +0200

    ADD: Registering Add Link Model State to state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import toggleButtonReducer from './toggleButtonSlice';
+import toggleAddLinkModelReducer from './toggleAddModelSlice';
+
 import userReducer from './userSlice';
+
 export default configureStore({
   reducer: {
     toggleButton:toggleButtonReducer,
     user:userReducer,
+    addLinkModel:toggleAddLinkModelReducer,
   },
 })
