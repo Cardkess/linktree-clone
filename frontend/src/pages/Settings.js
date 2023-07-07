a403889f8c14b6b69f099f12df7ef8e1341da2a3
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-07 19:12:28 +0200

    ADD: Axios and uploading settings

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,6 +1,60 @@
+import React, { useState } from "react";
 import logo from "../tappa.png";
 import { Form } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FaCheckCircle } from "react-icons/fa";
+import axios from "axios";
+
 export default function Settings() {
+  const user = useSelector((state) => state.user.value);
+  const [image, setImage] = useState(null);
+  const [formData, setFormData] = useState({
+    title: "",
+    subTitle: "",
+    backgroundColor: "",
+    theme: "light",
+  });
+  const [data, setData] = useState(null);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const url = process.env.REACT_APP_BACKEND_API_URL + "/settings";
+
+    const submission = {
+      title: formData.title,
+      subTitle: formData.subTitle,
+      backgroundColor: formData.backgroundColor,
+      theme: formData.theme,
+      image: image,
+    };
+
+    try {
+      const response = await axios.post(url, submission, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${user.token}`,
+        },
+      });
+
+      const responseData = response.data;
+      setData(responseData);
+    } catch (error) {
+      setData({ error: error.message });
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setImage(reader.result.split(",")[1]);
+      };
+    }
+  };
+
   return (
     <div className="setting-container">
       <div className="logo-container">
@@ -10,42 +64,81 @@ export default function Settings() {
       <div className="Setting-main">
         <h2>Settings</h2>
 
-        <Form className="settings-form">
+        <Form onSubmit={handleFormSubmit} className="settings-form">
           <div className="settings-form-group">
             <label>Page Title :</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={formData.title}
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+            />
           </div>
 
           <div className="settings-form-group">
             <label>Subtitle :</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={formData.subTitle}
+              onChange={(e) =>
+                setFormData({ ...formData, subTitle: e.target.value })
+              }
+            />
           </div>
 
           <div className="settings-form-group">
             <label>Background Color :</label>
-            <input type="color" id="colorPicker" name="colorPicker" />
+            <input
+              type="color"
+              id="colorPicker"
+              name="colorPicker"
+              value={formData.backgroundColor}
+              onChange={(e) =>
+                setFormData({ ...formData, backgroundColor: e.target.value })
+              }
+            />
           </div>
 
           <div className="settings-form-group">
-            <label for="themeSelect">Select a theme :</label>
-            <select id="themeSelect" name="theme">
+            <label htmlFor="themeSelect">Select a theme :</label>
+            <select
+              id="themeSelect"
+              name="theme"
+              value={formData.theme}
+              onChange={(e) =>
+                setFormData({ ...formData, theme: e.target.value })
+              }
+            >
               <option value="light">Light</option>
               <option value="dark">Dark</option>
             </select>
           </div>
 
           <div className="settings-form-group">
-            <label for="imageUpload">Upload Logo (.png) Image:</label>
+            <label htmlFor="imageUpload">Upload Logo (.png) Image:</label>
             <input
               type="file"
               id="imageUpload"
               name="imageUpload"
               accept="image/png"
+              onChange={handleFileChange}
             ></input>
           </div>
-          <div className="settings-form-group"><button className="save-settings-btn">Save Changes</button></div>
-          
+          <input type="hidden" name="token" value={user.token} />
+          <div className="settings-form-group">
+            <button className="save-settings-btn" type="submit">
+              Save Changes
+            </button>
+          </div>
         </Form>
+        {data && data.error && <span>{data.error}</span>}
+        {data && data.message && (
+          <div className="success-message-container">
+            <FaCheckCircle className="success-badge" />
+            <p className="success-message">{data.message}</p>
+          </div>
+        )}
       </div>
     </div>
   );
