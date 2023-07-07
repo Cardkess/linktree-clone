226015b2039f5367512a2d259153c7698a2f8f9e
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-07 22:24:17 +0200

    Bug Fix

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -39,7 +39,7 @@ export default function Settings() {
     } else {
       setFormData((prevFormData) => ({
         ...prevFormData,
-        backgroundColor: "black",
+        backgroundColor: "#000000",
       }));
     }
     if (settings && settings.theme) {
