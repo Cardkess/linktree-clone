f04c65718c94e90b1cee4a8404b25972c3146222
Blessings Chidambe <45852972+Cardkess@users.noreply.github.com>
2023-07-07 18:30:18 +0200

    ADD: Settings page UI

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,5 +1,52 @@
+import logo from "../tappa.png";
+import { Form } from "react-router-dom";
 export default function Settings() {
   return (
-    <div>Settings</div>
-  )
+    <div className="setting-container">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+
+      <div className="Setting-main">
+        <h2>Settings</h2>
+
+        <Form className="settings-form">
+          <div className="settings-form-group">
+            <label>Page Title :</label>
+            <input type="text" />
+          </div>
+
+          <div className="settings-form-group">
+            <label>Subtitle :</label>
+            <input type="text" />
+          </div>
+
+          <div className="settings-form-group">
+            <label>Background Color :</label>
+            <input type="color" id="colorPicker" name="colorPicker" />
+          </div>
+
+          <div className="settings-form-group">
+            <label for="themeSelect">Select a theme :</label>
+            <select id="themeSelect" name="theme">
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+            </select>
+          </div>
+
+          <div className="settings-form-group">
+            <label for="imageUpload">Upload Logo (.png) Image:</label>
+            <input
+              type="file"
+              id="imageUpload"
+              name="imageUpload"
+              accept="image/png"
+            ></input>
+          </div>
+          <div className="settings-form-group"><button className="save-settings-btn">Save Changes</button></div>
+          
+        </Form>
+      </div>
+    </div>
+  );
 }
